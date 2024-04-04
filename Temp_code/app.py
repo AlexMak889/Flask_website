3ffbfeec3f6a744582376641c404602fc8541264
Alex <alexmak889@gmail.com>
2024-04-04 19:58:20 +0200

    working conection betwen web server and raspberry pi And database is set upp

---
+++ Temp_code/app.py
@@ -0,0 +1,23 @@
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Store received data
+data_store = []
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    data = request.json
+    data_store.append(data)  # Store data
+    print('Received data:', data)
+    return jsonify({'message': 'Data received successfully'}), 200
+
+@app.route('/')
+def index():
+    
+    latest_data = data_store[-1] if data_store else {'temperature': 'N/A', 'humidity': 'N/A'}
+    return f"Temperature: {latest_data['temperature']}C, Humidity: {latest_data['humidity']}%"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  
