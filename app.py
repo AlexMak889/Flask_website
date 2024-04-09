8304e6c7f6c11bcb9a39bbc750535930f34549e6
Alex <alexmak889@gmail.com>
2024-04-09 19:32:28 +0200

    fix and add readme file

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     conn = mariadb.connect(
-        user="Alex",
-        password="alexmak889",
-        host="192.168.1.63",
-        database="temp"
+        user="username",
+        password="pasword",
+        host="raspberry pi ip adress",
+        database="database name"
     )
     cur = conn.cursor()
     cur.execute("SELECT * FROM sensor_data")
