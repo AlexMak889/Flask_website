b2625512aa643b54fb82434fdf96967cccdd3c17
Alex <alexmak889@gmail.com>
2024-04-19 17:39:30 +0200

    .

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template, request
-import mariadb
 import db
-import matplotlib_code
 from datetime import timedelta
 import matplotlib.pyplot as plt
 import io
 import random
+import numpy as np 
+from matplotlib.figure import Figure
 from flask import Flask, Response, request
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.backends.backend_svg import FigureCanvasSVG
-
-
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+def graph(num_x_points=50):
+    """ Renders the plot on the fly."""
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    x_points = range(num_x_points)
+    axis.plot(x_points, [random.randint(1, 30) for x in x_points])
 
-
-
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return render_template('temp_code_week.html', image=output.getvalue())
 
 app = Flask(__name__)
 
 @app.route('/', methods=['get'])
 def index():
-    num_x_points = int(request.args.get("num_x_points", 50))
     conn = db.conn
     cur = conn.cursor()
     cur.execute("SELECT * FROM sensor_data")
@@ -35,20 +40,12 @@ def index():
         processed_data.append(tuple(formatted_row))
 
     return render_template('index.html', data=processed_data)
-    
-@app.route('/temp this week', methods=['GET'])
-def index2(num_x_points=50):
-    """ renders the plot on the fly.
-    """
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    x_points = range(num_x_points)
-    axis.plot(x_points, [random.randint(1, 30) for x in x_points])
 
-    output = io.BytesIO()
-    FigureCanvasSVG(fig).print_svg(output)
-    return Response(output.getvalue(), mimetype="image/svg+xml")
-   #return render_template('temp_code_week.html')
+@app.route('/day', methods=['GET'])
+def index2():
+    plot = graph(num_x_points=50)
+    plot.savefig(os.path.join('static', 'images', 'plot.png'))     
+    return render_template('day.html') 
 
 if __name__ == '__main__':
     import webbrowser
