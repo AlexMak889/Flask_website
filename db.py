a7b22c844d5b7f1543e4cc3e238fadefdee9aec6
Alex <alexmak889@gmail.com>
2024-04-19 16:43:42 +0200

    .

--- db.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-import mariadb
-
-conn = mariadb.connect(
-        user="Alex",
-        password="alexmak889",
-        host="192.168.1.63",
-        database="temp"
-    )
