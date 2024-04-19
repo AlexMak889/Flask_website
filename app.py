f968b74c5b8e6f6bf945deb0f25f042af9b12fd1
Alex <alexmak889@gmail.com>
2024-04-19 16:39:53 +0200

    new branch

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mariadb
 import db
+import matplotlib_code
+from datetime import timedelta
+import matplotlib.pyplot as plt
+
+
+
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['get'])
 def index():
     conn = db.conn
     cur = conn.cursor()
@@ -11,8 +18,33 @@ def index():
     data = cur.fetchall()
     cur.close()
     conn.close()
+    processed_data = []
+    for row in data:
+        formatted_row = list(row)
+        formatted_row[2] = str(timedelta(seconds=row[2].seconds))
+        processed_data.append(tuple(formatted_row))
 
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=processed_data)
+@app.route('/temp this week', methods=['GET'])
+def index2():
+    if request.method =='GET':
+        data_x = index2()
+        print("data_y length")
+        print(len(data_x))
+        data_y = index()
+        print(len(data_y))
+        x = list(range(1, len(data_x) + 1))
+        y = list(range(1, len(data_y) + 1))
+
+
+        plt.plot(data_x, data_y)  
+
+        plt.xlabel('Index')
+        plt.ylabel('Temperature')
+        plt.title(label="Sensor Data", fontsize=16, color="green")
+        plt.legend()
+        plt.show()
+    else:return render_template('temp_code_week.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- matplotlib_code.py
+++ matplotlib_code.py
@@ -4,21 +4,36 @@ import db
 def index():
     conn = db.conn
     cur = conn.cursor()
-    cur.execute("SELECT temp FROM sensor_data")
-    data = cur.fetchall()
+    cur.execute("SELECT temp FROM sensor_data WHERE temp_date BETWEEN '2024-01-01' AND '2024-12-31' AND temp_time BETWEEN '12:00:00' AND '12:10:00'")
+    data = [item[0] for item in cur.fetchall()]
     cur.close()
-    conn.close()
+    # conn.close()  # Consider uncommenting this line to close the connection
+    return data
 
-    return [item[0] for item in data]  # Extracting values from tuples
+def index2():
+    conn = db.conn
+    cur = conn.cursor()
+    cur.execute("SELECT temp_date FROM sensor_data WHERE temp_date BETWEEN '2024-01-01' AND '2024-12-31' AND temp_time BETWEEN '12:00:00' AND '12:10:00'")
+    data = [item[0] for item in cur.fetchall()]
+    cur.close()
+    # conn.close()  # Consider uncommenting this line to close the connection
+    print(data)
 
-data = index()
+    return data
 
-# Generating x values (assuming sequential)
-x = list(range(1, len(data) + 1))
+data_x = index2()
+print("data_y length")
+print(len(data_x))
+data_y = index()
+print(len(data_y))
+x = list(range(1, len(data_x) + 1))
+y = list(range(1, len(data_y) + 1))
 
-# Plotting the data
-plt.plot(x, data, color='green')
+
+plt.plot(data_x, data_y)  
+
 plt.xlabel('Index')
 plt.ylabel('Temperature')
 plt.title(label="Sensor Data", fontsize=16, color="green")
+plt.legend()
 plt.show()
